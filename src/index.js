756c4edf41e6766d2f49f75a45c2c31911e34cd9
Valentin-KV <krivenko.vg@yandex.ru>
2023-01-14 00:51:20 +0300

    init: start project revers-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
-}
+  let resN = Math.abs(n);
+  const stringRes = String(resN).split('').reverse().join('');
+  return +stringRes;
+};
+module.exports();
